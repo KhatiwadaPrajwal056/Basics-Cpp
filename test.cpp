6158c79129504e91be94fc3fed4f38790db998dc
KhatiwadaPrajwal056 <khatiwadaprajwal22@icloud.com>
2023-06-19 15:39:41 +0545

    C++

--- test.cpp
+++ test.cpp
@@ -1,6 +1,46 @@
 #include<iostream>
 using namespace std;
-int main()
-{
+int add(int [],int [],int [],int);
+int main(){
+    int n;
+    cout<<"Enter number of bits: ";
+    cin>>n;
+    int sum[n];
+    int *b1= new int[n];
+    int *b2= new int[n];
+    cout<<"Enter b1: ";
+    for(int i=0;i<n;i++){
+        cin>>b1[i];
+    }
+    cout<<"Enter b2: ";
+    for(int i=0;i<n;i++){
+        cin>>b2[i];
+    }
+    int i=n;
+    do{
+        i--;
+        b2[i]=b2[i];
+    }while(b2[i]!=1);
+    for(i=i-1;i>=0;i--){
+        if(b2[i]==0){
+            b2[i]=1;
+        }
+        else
+            b2[i]=0;
+    }
+    int carry=add(b1,b2,sum,n);
+    cout<<"Carry is: "<<carry<<endl;
+    cout<<"Sum is: ";
+    for(int i=0;i<n;i++){
+        cout<<sum[i];
+    }
     return 0 ;
+}
+int add(int *b1,int *b2,int *sum,int n){
+    int carry=0;
+    for (int i=n-1;i>=0;i--){
+        sum[i]=(b1[i]+b2[i]+carry)%2;
+        carry=(b1[i]+b2[i]+carry)/2;
+    }
+    return carry;
 }
--- trest.cpp
+++ trest.cpp
@@ -1,10 +1,11 @@
 #include<iostream>
 using namespace std;
 int main(){
-    int x=9 ,z =0;
-    int *y = &z; // pointer
-    cout<<x<<endl;
-    cout<<&x<<endl;
-    cout <<*y<< endl; // dereferencing of pointer
+    int i=3;
+    do {
+        i--;
+        
+        cout<<"Pk";
+    }while(i!=1);
     
 }
