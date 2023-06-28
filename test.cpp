f047e5eaaadf8905d8cfcfd87c6e842543c1bfce
KhatiwadaPrajwal056 <khatiwadaprajwal22@icloud.com>
2023-06-28 16:49:57 +0545

    practice

--- test.cpp
+++ test.cpp
@@ -2,6 +2,7 @@
 #include<iostream>
 using namespace std;
 int add(int [],int [],int [],int);
+int twos(int [],int [],int [],int);
 int main(){
     int n;
     cout<<"Enter number of bits: ";
@@ -17,25 +18,38 @@ int main(){
     for(int i=0;i<n;i++){
         cin>>b2[i];
     }
-    int i=n;
-    do{
-        i--;
-        b2[i]=b2[i];
-    }while(b2[i]!=1);
-    for(i=i-1;i>=0;i--){
-        if(b2[i]==0){
-            b2[i]=1;
-        }
-        else
-            b2[i]=0;
-    }
-    int carry=add(b1,b2,sum,n);
+    int carry=twos(b1,b2,sum,n);
     cout<<"Carry is: "<<carry<<endl;
     cout<<"Sum is: ";
-    for(int i=0;i<n;i++){
-        cout<<sum[i];
+    if(carry){
+        for(int i=0;i<n;i++){
+        cout<<sum[i]; }
+    }
+    else{
+        int *newo=new int [n];
+        twos(b1,sum,newo,n);
+        cout<<"-";
+        for(int i=0;i<n;i++){
+            cout<<newo[i];
+        }
     }
     return 0 ;
+}
+int twos(int *b1,int *b2,int sum,int n){
+        int *ret=new int [n];
+        int i=n;
+        do{
+            i--;
+            b2[i]=b2[i];
+        }while(b2[i]!=1);
+        for(i=i-1;i>=0;i--){
+            if(b2[i]==0){
+                b2[i]=1;
+            }
+            else
+                b2[i]=0;
+        }
+        return add(b1,b2,ret,n);
 }
 int add(int *b1,int *b2,int *sum,int n){
     int carry=0;
@@ -45,3 +59,5 @@ int add(int *b1,int *b2,int *sum,int n){
     }
     return carry;
 }
+
+
